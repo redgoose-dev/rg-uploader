e1cfffbe5ca8ab58e85e0214a65452d9bdfe0303
redgoose <scripter@me.com>
2017-01-20 13:53:44 +0900

    Update vendors & package

--- vendor/Croppie/deploy.js
+++
@@ -1,7 +0,0 @@
-var ghpages = require('gh-pages'),
-    path = require('path');
-
-ghpages.publish(__dirname, function (err) {
-   if (err) console.error(err);
-   else console.log('Successfully deployed to gh-pages');
-});
